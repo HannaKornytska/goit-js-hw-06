4b96976aa418e2888519957d70b4510a9c64c771
HannaKornytska <annasmyk222@gmail.com>
2023-05-09 14:35:56 +0100

    update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,13 +1,16 @@
-const validatorInput = document.querySelector('#validation-input');
-const inputValid = document.querySelector('.valid');
-const inputInvalid = document.querySelector('.invalid');
+const inputRef = document.querySelector('#validation-input');
 
+inputRef.addEventListener('blur', onValidButtonBlur);
 
-validatorInput.addEventListener('blur',uncheckValidInput);
+function onValidButtonBlur(event) {
+    inputRef.value.length >= parseInt(inputRef.dataset.length) ? isValueValid() : isValueInvalid();
+};
 
-function uncheckValidInput(event) {
-    
+function isValueValid() {
+    inputRef.classList.contains('invalid') ? inputRef.classList.replace('invalid', 'valid') : inputRef.classList.add('valid');
+};
 
-}
-
+function isValueInvalid() {
+    inputRef.classList.contains('valid') ? inputRef.classList.replace('valid', 'invalid') : inputRef.classList.add('invalid');
+};
 
